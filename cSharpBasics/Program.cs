4a8023340e42bd58b8c24655c3048679c6bbc628
Madhura <madhura.bhujbal@gmail.com>
2020-12-10 14:31:54 +0000

    Practice examples for literal data types

--- cSharpBasics/Program.cs
+++ cSharpBasics/Program.cs
@@ -7,9 +7,15 @@ namespace basicC_
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
-            Console.Write("Congratulations!");
-            Console.Write(" ");
-            Console.Write("You wrote your first lines of code");
+            // Console.Write("Congratulations!");
+            // Console.Write(" ");
+            // Console.Write("You wrote your first lines of code");
+
+            Console.WriteLine('b');
+            Console.WriteLine(123);
+            Console.WriteLine(12.3);
+            Console.WriteLine(true);
+            Console.WriteLine(false);
         }
     }
 }
