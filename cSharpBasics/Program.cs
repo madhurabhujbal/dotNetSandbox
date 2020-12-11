3a14d92f1219e01ff62d9c0b1f19a53f7b2ccc28
Madhura <madhura.bhujbal@gmail.com>
2020-12-11 12:04:44 +0000

    Practice code for \t and \n escape sequences

--- cSharpBasics/Program.cs
+++ cSharpBasics/Program.cs
@@ -6,32 +6,8 @@ namespace basicC_
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
-            // Console.Write("Congratulations!");
-            // Console.Write(" ");
-            // Console.Write("You wrote your first lines of code");
-
-            // Console.WriteLine('b');
-            // Console.WriteLine(123);
-            // Console.WriteLine(12.3);
-            // Console.WriteLine(true);
-            // Console.WriteLine(false);
-
-            // string firstName;
-            // firstName = "Bob";
-            // Console.WriteLine(firstName);
-            // firstName = "Rob";
-            // Console.WriteLine(firstName);
-            // firstName = "Tom";
-            // Console.WriteLine(firstName);
-
-            var message = 123;
-            Console.Write(message);
-
-            string name = "Bob";
-            int quantity = 3;
-            decimal temperature = 34.4m;
-            Console.WriteLine($"Hello, {name}! You have {quantity} in your inbox. The temperature is {temperature} celsius. ");
+            Console.WriteLine("Hello\tWorld!");
+            Console.WriteLine("Hello\nWorld!");
         }
     }
 }
