f885335a972e4e2b0a17ef9a02943cb230d020ce
Madhura <madhura.bhujbal@gmail.com>
2020-12-13 22:12:31 +0000

    Practice code to perform basic math operations

--- cSharpBasics/Program.cs
+++ cSharpBasics/Program.cs
@@ -10,6 +10,16 @@ namespace basicC_
             int widgetsSold = 7;
             Console.WriteLine(firstName + " sold " + widgetsSold + 7 + " widgets.");
             Console.WriteLine(firstName + " sold " + (widgetsSold + 7) + " widgets.");
+
+            int sum = 7 + 5;
+            int difference = 7 - 5;
+            int product = 7 * 5;
+            int quotient = 7 / 5;
+
+            Console.WriteLine("Sum: " + sum);
+            Console.WriteLine("Difference: " + difference);
+            Console.WriteLine("Product: " + product);
+            Console.WriteLine("Quotient: " + quotient);
         }
     }
 }
