b383b2d5ff17a8dd68298ccdf71e90320f060a59
Madhura <madhura.bhujbal@gmail.com>
2020-12-14 11:09:14 +0000

    Practice code for increment/decrement operators

--- cSharpBasics/Program.cs
+++ cSharpBasics/Program.cs
@@ -45,6 +45,32 @@ namespace basicC_
             Console.WriteLine(value2);
             Console.WriteLine(value3);
 
+            //Increment and decrement operators
+            int value = 1;
+            value = value + 1;
+            Console.WriteLine("First increment: " + value);
+
+            value += 1;
+            Console.WriteLine("Second increment: " + value);
+
+            value++;
+            Console.WriteLine("Third increment: " + value);
+
+            value = value - 1;
+            Console.WriteLine("First decrement: " + value);
+
+            value -= 1;
+            Console.WriteLine("Second decrement: " + value);
+
+            value--;
+            Console.WriteLine("Third decrement: " + value);
+
+            Console.WriteLine("Post increment: " + (value++));
+            Console.WriteLine("Pre increment: " + (++value));
+            Console.WriteLine("Pre decrement: " + (--value));
+            Console.WriteLine("Post decrement: " + (value--));
+
+
         }
     }
 }
