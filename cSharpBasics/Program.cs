1c6d07ca8e56b8091e324f06d31cfbe4c3d0c109
Madhura <madhura.bhujbal@gmail.com>
2020-12-17 11:46:33 +0000

    Writing condition using if-else and nested loop

--- cSharpBasics/Program.cs
+++ cSharpBasics/Program.cs
@@ -7,24 +7,32 @@ namespace basicC_
         static void Main(string[] args)
         {
             Random dice = new Random(); //stateful method
-            int roll1 = dice.Next(1, 7);
-            int roll2 = dice.Next(1, 7);
-            int roll3 = dice.Next(1, 7);
+            int roll1 = 6;
+            int roll2 = 6;
+            int roll3 = 6;
 
             int total = roll1 + roll2 + roll3;
             Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total} ");
 
             if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
             {
-                Console.WriteLine("You rolled doubles! +2 bonus to total");
-                total += 2;
+                if ((roll1 == roll2) && (roll2 == roll3))
+                {
+                    Console.WriteLine("You rolled triples! +6 bonus to total!");
+                    total += 6;
+                }
+                else
+                {
+                    Console.WriteLine("You rolled doubles! +2 bonus to total!");
+                    total += 2;
+                }
             }
 
             if (total >= 15)
             {
                 Console.WriteLine("You win!!");
             }
-            if (total < 15)
+            else
             {
                 Console.WriteLine("Sorry, you lose");
             }
