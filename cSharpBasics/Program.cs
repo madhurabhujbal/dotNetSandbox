5ae7f2e79714bcb03f5abc32890de3ce99b01c52
Madhura <madhura.bhujbal@gmail.com>
2020-12-19 19:41:51 +0000

    Practice code for initailising and iterating over an array

--- cSharpBasics/Program.cs
+++ cSharpBasics/Program.cs
@@ -6,43 +6,14 @@ namespace basicC_
     {
         static void Main(string[] args)
         {
-            Random dice = new Random(); //stateful method
-            int roll1 = dice.Next(1, 7);
-            int roll2 = dice.Next(1, 7);
-            int roll3 = dice.Next(1, 7);
-
-            int total = roll1 + roll2 + roll3;
-            Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total} ");
-
-            if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+            //Initialisation of array
+            string[] stringArray = new string[3];
+            stringArray[0] = "ABC";
+            stringArray[1] = "FFD";
+            stringArray[2] = "ETD";
+            foreach (string s in stringArray)
             {
-                if ((roll1 == roll2) && (roll2 == roll3))
-                {
-                    Console.WriteLine("You rolled triples! +6 bonus to total!");
-                    total += 6;
-                }
-                else
-                {
-                    Console.WriteLine("You rolled doubles! +2 bonus to total!");
-                    total += 2;
-                }
-            }
-
-            if (total >= 16)
-            {
-                Console.WriteLine("You win a new car!");
-            }
-            else if (total >= 10)
-            {
-                Console.WriteLine("You win a new laptop!");
-            }
-            else if (total == 7)
-            {
-                Console.WriteLine("You win a trip!");
-            }
-            else
-            {
-                Console.WriteLine("You win a kitten!");
+                Console.WriteLine(s);
             }
         }
     }
