bed0e894fe4a9e2705725814314c730cefb19a0e
Madhura <madhura.bhujbal@gmail.com>
2020-12-20 11:14:59 +0000

    Practice code for initialising and assigning values in array at same time

--- cSharpBasics/Program.cs
+++ cSharpBasics/Program.cs
@@ -15,6 +15,18 @@ namespace basicC_
             {
                 Console.WriteLine(s);
             }
+
+            //Initialise and add values in array at same time
+            int[] inverntory = { 200, 450, 700, 175, 250 };
+            int sum = 0;
+            int bin = 0;
+            foreach (int items in inverntory)
+            {
+                sum += items;
+                bin++;
+                Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum}) ");
+            }
+            Console.WriteLine($"We have {sum} items in inventory.");
         }
     }
 }
