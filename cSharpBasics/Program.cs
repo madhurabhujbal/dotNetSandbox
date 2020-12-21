39ef86bd00d3cd099bb0ab6245ab0bb3ead1d06e
Madhura <madhura.bhujbal@gmail.com>
2020-12-21 23:47:33 +0000

    Practice code for != operator

--- cSharpBasics/Program.cs
+++ cSharpBasics/Program.cs
@@ -6,12 +6,19 @@ namespace basicC_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("a" == "a");
-            Console.WriteLine("a" == "A");
-            Console.WriteLine(1 == 2);
+            // Console.WriteLine("a" == "a");
+            // Console.WriteLine("a" == "A");
+            // Console.WriteLine(1 == 2);
+
+            // string myValue = "a";
+            // Console.WriteLine(myValue == "a");
+
+            Console.WriteLine("a" != "a");
+            Console.WriteLine("a" != "A");
+            Console.WriteLine(1 != 2);
 
             string myValue = "a";
-            Console.WriteLine(myValue == "a");
+            Console.WriteLine(myValue != "a");
         }
     }
 }
