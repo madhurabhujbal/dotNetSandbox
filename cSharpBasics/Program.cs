ccfe0dd18759d3491080f1908d6e652cd612946a
Madhura <madhura.bhujbal@gmail.com>
2020-12-21 23:52:31 +0000

    Practice code for comparison operators

--- cSharpBasics/Program.cs
+++ cSharpBasics/Program.cs
@@ -13,12 +13,16 @@ namespace basicC_
             // string myValue = "a";
             // Console.WriteLine(myValue == "a");
 
-            Console.WriteLine("a" != "a");
-            Console.WriteLine("a" != "A");
-            Console.WriteLine(1 != 2);
+            // Console.WriteLine("a" != "a");
+            // Console.WriteLine("a" != "A");
+            // Console.WriteLine(1 != 2);
+            // string myValue = "a";
+            // Console.WriteLine(myValue != "a");
 
-            string myValue = "a";
-            Console.WriteLine(myValue != "a");
+            Console.WriteLine(1 > 2);
+            Console.WriteLine(1 < 2);
+            Console.WriteLine(1 >= 1);
+            Console.WriteLine(1 <= 1);
         }
     }
 }
