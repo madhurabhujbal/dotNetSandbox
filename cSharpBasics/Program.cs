c37c679a67bab9c7c422a0ea4ae908d58b81cd9a
Madhura <madhura.bhujbal@gmail.com>
2020-12-21 21:52:07 +0000

    Practice code to understand equality operator

--- cSharpBasics/Program.cs
+++ cSharpBasics/Program.cs
@@ -6,27 +6,12 @@ namespace basicC_
     {
         static void Main(string[] args)
         {
-            //Initialisation of array
-            string[] stringArray = new string[3];
-            stringArray[0] = "ABC";
-            stringArray[1] = "FFD";
-            stringArray[2] = "ETD";
-            foreach (string s in stringArray)
-            {
-                Console.WriteLine(s);
-            }
+            Console.WriteLine("a" == "a");
+            Console.WriteLine("a" == "A");
+            Console.WriteLine(1 == 2);
 
-            //Initialise and add values in array at same time
-            int[] inverntory = { 200, 450, 700, 175, 250 };
-            int sum = 0;
-            int bin = 0;
-            foreach (int items in inverntory)
-            {
-                sum += items;
-                bin++;
-                Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum}) ");
-            }
-            Console.WriteLine($"We have {sum} items in inventory.");
+            string myValue = "a";
+            Console.WriteLine(myValue == "a");
         }
     }
 }
