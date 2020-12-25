d2db38a49ad0a1a6973b8cc3500bef1045621777
Madhura <madhura.bhujbal@gmail.com>
2020-12-25 17:57:47 +0000

    Practice code for variable scope declared inside a function

--- cSharpBasics/Program.cs
+++ cSharpBasics/Program.cs
@@ -6,13 +6,12 @@ namespace basicC_
     {
         static void Main(string[] args)
         {
-            int saleAmount = 1001;
-            int discount = saleAmount > 1000 ? 100 : 50;
-            Console.WriteLine($"Discount: {discount}");
-
-            Random coin = new Random();
-            int flipResult = coin.Next(0, 2);
-            Console.WriteLine((flipResult == 0) ? "heads" : "tails");
+            bool flag = true;
+            if (flag)
+            {
+                int value = 10;
+                Console.WriteLine($"Inside of code block: {value}");
+            }
         }
     }
 }
