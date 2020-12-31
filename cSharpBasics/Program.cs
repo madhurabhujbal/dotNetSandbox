ab4e8345b0bd9c6720492140551b74bd56943356
Madhura <madhura.bhujbal@gmail.com>
2020-12-31 12:15:31 +0000

    Rearrange code for better understanding

--- cSharpBasics/Program.cs
+++ cSharpBasics/Program.cs
@@ -9,7 +9,10 @@ namespace basicC_
             string[] names = { "Alex", "Eddie", "David", "Michael" };
             for (int i = 0; i < names.Length; i++)
             {
-                if (names[i] == "David") names[i] = "Sammy";
+                if (names[i] == "David")
+                {
+                    names[i] = "Sammy";
+                }
             }
             foreach (var name in names) Console.WriteLine(name);
         }
