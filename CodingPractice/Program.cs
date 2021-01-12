cf90f165aabf25258169555cfc739d9be7a9c652
Madhura <madhura.bhujbal@gmail.com>
2021-01-12 16:22:04 +0000

    Refactor the code

--- CodingPractice/Program.cs
+++ CodingPractice/Program.cs
@@ -5,7 +5,14 @@ namespace CodingPractice
     class Program
     {
 
-        private static bool isConditionA(int numValue)
+        //Initialise counts
+        int countA = 0;
+        int countB = 0;
+        int countAB = 0;
+        int None = 0;
+        bool conditionA;
+        bool conditionB;
+        private bool isConditionA(int numValue)
         {
             if ((numValue >= 1 && numValue <= 3) || (numValue >= 5 && numValue <= 7))
             {
@@ -14,7 +21,7 @@ namespace CodingPractice
             return false;
         }
 
-        private static bool isConditionB(int numValue)
+        private bool isConditionB(int numValue)
         {
             if ((numValue >= 6 && numValue <= 11) || (numValue >= 33 && numValue <= 44))
             {
@@ -23,51 +30,58 @@ namespace CodingPractice
             return false;
         }
 
+        private void processLine(string currLine)
+        {
+            conditionA = false;
+            conditionB = false;
+            string[] elements = currLine.Split(',');
+            foreach (string element in elements)
+            {
+                int numValue = int.Parse(element);
+                conditionA = conditionA || isConditionA(numValue);
+                conditionB = conditionB || isConditionB(numValue);
+            }
+            updateCounts();
+        }
+
+        private void updateCounts()
+        {
+            // Update count based on a condition
+            if (conditionA && conditionB)
+            {
+                countAB++;
+            }
+            else if (conditionA)
+            {
+                countA++;
+            }
+            else if (conditionB)
+            {
+                countB++;
+            }
+            else
+            {
+                None++;
+            }
+        }
+
+        private void printSummary()
+        {
+            Console.WriteLine($" Only condition A: {countA} \n Only condition B: {countB} \n Both condition A & B: {countAB} \n None: {None}");
+
+        }
         static void Main(string[] args)
         {
 
+            Program obj = new Program();
             string[] numberArray = System.IO.File.ReadAllLines(@"C:\C#\CodingPractice\Input.txt");
-            //Initialise counts
-            int countA = 0;
-            int countB = 0;
-            int countAB = 0;
-            int None = 0;
 
             foreach (string currLine in numberArray)
             {
-                bool conditionA = false;
-                bool conditionB = false;
-
                 // Check for conditions
-                string[] elements = currLine.Split(',');
-                foreach (string element in elements)
-                {
-                    int numValue = int.Parse(element);
-                    conditionA = conditionA || isConditionA(numValue);
-                    conditionB = conditionB || isConditionB(numValue);
-                }
-
-                // Update count based on a condition
-                if (conditionA && conditionB)
-                {
-                    countAB++;
-                }
-                else if (conditionA)
-                {
-                    countA++;
-                }
-                else if (conditionB)
-                {
-                    countB++;
-                }
-                else
-                {
-                    None++;
-                }
+                obj.processLine(currLine);
             }
-
-            Console.WriteLine($" Only condition A: {countA} \n Only condition B: {countB} \n Both condition A & B: {countAB} \n None: {None}");
-
+            obj.printSummary();
         }
     }
 }
