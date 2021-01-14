4e21840e62a82a47adb171e27005cd475e6d567a
Madhura <madhura.bhujbal@gmail.com>
2021-01-14 10:30:35 +0000

    Correct the condition for running Fibonacci series

--- FibonacciSeries/Program.cs
+++ FibonacciSeries/Program.cs
@@ -15,7 +15,7 @@ namespace FibonacciSeries
             int n2 = 1;
             int sum = 0;
 
-            for (int i = 2; i < n; i++)
+            for (int i = 2; i <= n; i++)
             {
                 sum = n1 + n2;
                 n1 = n2;
